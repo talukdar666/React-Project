ae8cc8679f8ee268bf4129f1bc3747ce76e244cb
talukdar666 <luciferboi6617@gmail.com>
2023-02-03 14:49:57 +0600

    #30

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div className="App">
+      <center>
+        <h1>Start of the Project</h1>
+        <hr />
+      </center>
+    </div>
+  );
+}
+
+export default App;
